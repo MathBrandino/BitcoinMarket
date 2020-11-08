8258b4be037ff6326194a7dd0eab39846d058e2c
Matheus Brandino <m.v.brandino@gmail.com>
2020-11-08 12:10:23 -0300

    make service receives the chart's kind

--- app/src/main/java/com/mathbrandino/bitcoin_market/data/network/BitcoinService.kt
+++ app/src/main/java/com/mathbrandino/bitcoin_market/data/network/BitcoinService.kt
@@ -3,8 +3,9 @@ package com.mathbrandino.bitcoin_market.data.network
 import com.mathbrandino.bitcoin_market.data.model.BitcoinChartResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BitcoinService {
-    @GET("/charts/market-price")
-    fun fetchChart(): Single<BitcoinChartResponse>
+    @GET("/charts/{kind}")
+    fun fetchChart(@Path("kind") kind: String): Single<BitcoinChartResponse>
 }
